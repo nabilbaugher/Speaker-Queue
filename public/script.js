bb93c7ccc46a0c56cfcce867f2f8dd8f0813eea6
Nabil <nabilbaugher@gmail.com>
2020-08-10 11:11:40 -0500

    Added light/dark mode toggle

--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
 const el = document.getElementById("draggable_list");
 const sortable = Sortable.create(el);
 
+// add proper capitalization to the list of submitted strings
 function capitalize(string) {
     let result = "";
     for (x of string.split(" ")) {
-        result += x.charAt(0).toUpperCase() + x.slice(1) + " ";
+        result += x.charAt(0).toUpperCase() + x.slice(1).toLowerCase() + " ";
     }
     return result.slice(0, result.length);
 }
 
+// add a name to the end of the list
 function add(name) {
     $("#draggable_list").append(
         "<li class=\"list-group-item\">" + 
@@ -19,15 +21,18 @@ function add(name) {
     );
 }
 
+// remove element from list
 function remove(btn) {
     btn.parentNode.parentNode.removeChild(btn.parentNode);
 }
 
+// remove element from list and add it at the bottom
 function requeue(btn) {
     remove(btn);
     $("#draggable_list").append(btn.parentNode);
 }
 
+// when the user presses enter, add their submissions to the list and clear the textareas
 $("#input_names").keyup(function(event) {
     if (event.which === 13) {
         console.log("names recieved");
@@ -42,5 +47,17 @@ $("#input_names").keyup(function(event) {
             add(name);
         }
         $("#input_names").val("");
+    }
+})
+
+// light/dark mode toggle
+$("#toggle").change(function() {
+    console.log("beand");
+    if (this.checked) {
+        $("#light_mode").remove();
+        $("head").append('<link id="dark_mode" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css" integrity="sha384-Bo21yfmmZuXwcN/9vKrA5jPUMhr7znVBBeLxT9MA4r2BchhusfJ6+n8TLGUcRAtL" crossorigin="anonymous">');
+    } else {
+        $("#dark_mode").remove();
+        $("head").append('<link id="light_mode" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/flatly/bootstrap.min.css">');
     }
 })
